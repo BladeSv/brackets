3c26988f05613adde23f86ea98f2ec643e9b1ba3
Mihail Mitrahovich <mikhail_mitrakhovich@epam.com>
2021-03-12 02:15:14 +0300

    add method implimentation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (!str || !bracketsConfig || !bracketsConfig.length || str.length % 2 !== 0) {
+    return false;
+  }
+  const strArr = str.split("");
+  let startLength;
+
+  while (startLength !== strArr.length) {
+    startLength = strArr.length;
+
+    for (let i = 0; i < strArr.length - 1 && i >= 0; i++) {
+      const bracket = bracketsConfig.find(el => strArr[i] === el[0]);
+      if (bracket && bracket[1] === strArr[i + 1]) {
+        strArr.splice(i, 2);
+        i -= 2;
+      }
+    }
+  }
+
+  return !strArr.length;
 }
